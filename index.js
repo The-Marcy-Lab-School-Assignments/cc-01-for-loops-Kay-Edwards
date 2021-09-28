7b605ffc5c6ed6c79a224524156dc057fc234689
Kayla Edwards <37392966+Kay-Edwards@users.noreply.github.com>
2021-09-28 18:14:48 -0400

    code challenge day 1

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 //1
-function countToFifty(){
-  
+function morningLogged(){
+  console.log('Good Morning');
 }
 
 //2
-function countFromOne(){
-  
+function iLoveCode(){
+  return 'I love code challenges';
 }
 
 //3
-function threeAndFiveInFifty(){
-  
+function loggingTwice(){
+  console.log('one');
+  console.log('two');
 }
 
 //4
-function threeAndFive(){
+function noStep(){
+  console.log('one');
+  return 'two';
+  console.log(step);
+}
+
+//5
+function isPrime(){
   
 }
 
 
+morningLogged();
+console.log(iLoveCode());
+loggingTwice();
+noStep();
